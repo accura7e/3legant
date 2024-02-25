f9ab4fa139f62148a383c43bf25b04c50e9298e3
accura7e <accura7ee@gmail.com>
2024-02-25 11:10:54 +0300

    add: added nav

--- build/js/index.bundle.js
+++ build/js/index.bundle.js
@@ -1,1 +1,1 @@
-!function(){"use strict";!function(){const o=document.querySelector(".mobile-nav-btn"),e=document.querySelector(".mobile-nav"),n=document.querySelector(".nav-icon");o.onclick=function(){e.classList.toggle("mobile-nav--open"),n.classList.toggle("nav-icon--active"),document.body.classList.toggle("no-scroll")}}()}();
+!function(){"use strict";!function(){const e=document.querySelector(".tagline__close"),c=document.querySelector(".tagline");e.onclick=function(){c.remove()}}()}();
